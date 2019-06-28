b63b38151841c51fcdcef1b45405e1015da0f9ac
Emmanuel <emmanuel.arbouch@unistellaroptics.com>
2019-06-28 17:38:32 +0200

    add customize.sh and requirements.txt

---
+++ customize.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get install -y python3.7 python3-pip virtualenv build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libgeos-dev libxml2 libffi-dev libssl-dev
+
+# pip for python3.7
+curl https://bootstrap.pypa.io/get-pip.py | sudo -H python3.7
+    
+python3.7 -m pip install --user virtualenvwrapper
+
+export PATH=$PATH:"$HOME"/.local/bin
+export WORKON_HOME="$HOME"/venvs
+
+if [[ ! -e $WORKON_HOME ]]; then
+    mkdir -p $WORKON_HOME
+elif [[ ! -d $WORKON_HOME ]]; then
+    echo "$WORKON_HOME already exists but is not a directory" 1>&2
+fi
+
+export VIRTUALENVWRAPPER_PYTHON="$(command \which python3.7)"
+source "$HOME"/.local/bin/virtualenvwrapper.sh
+
+mkvirtualenv 3.7 -p python3.7 -r "$PWD"/requirements.txt
+
+echo "done with python-insntall !"
+
